b907874cd30e0ce2fc8a25f8d9a821cd95f5966b
Mishab <85479103+imishab@users.noreply.github.com>
2022-11-05 13:59:48 +0000

    Update urlConfig.js

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,5 @@
 const baseUrl =
-  location.hostname === "localhost"
+ location && location.hostname === "localhost"
     ? "http://localhost:2000"
     : "https://forskor-rest-server.herokuapp.com";
 
